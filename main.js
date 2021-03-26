d39e92866e6b440d126bda0577c5bb2830483384
Intra <shsscpg131@gmail.com>
2021-03-26 21:10:52 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+window.onload=function(){
+console.log('checking');
+  var checkExist = setInterval(function() {
+   if ($('.gs-title').length) {
+      console.log("Exists!");
+       var cookieValue = document.getElementByClassName('gs-title');
+   console.log(cookieValue);
+      clearInterval(checkExist);
+   }
+}, 100); // check every 100ms
+  }*/
